0e922b35bccdba1507ed4f22e54c9f1de5875cef
Serjoscha Fanger <Serjoscha.Fanger@gmx.de>
2018-12-18 10:12:12 +0100

    addtion of the isLeapyear function, which determines if input year is a leap year

--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// I was here - SFanger
-
 /**
  *
  *
@@ -17,6 +15,35 @@ int day_of_the_year(int day, int month, int year)
 {
     // Returns now 1 instead of 0, what a change
     return 1;
+}
+
+/**
+* Name : isLeapyear
+* Param : int year
+* Returns :
+*           0, if year is not a leap year
+*           1, if year is a leap year
+*          -1, if year is smaller 1582
+* Funtion : Checks if year is a leap year in the gregorian calender
+**/
+int isLeapyear(int year)
+{
+    if (year < 1582)
+    {
+        return -1;
+    }
+    if (year%4 != 0)
+    {
+        return 0;
+    }
+    else if (year%100 != 0 || year%400 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int main()
