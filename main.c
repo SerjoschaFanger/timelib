1d1c163abe860bbf5b9046d3f54ebe6bed42a348
Serjoscha Fanger <Serjoscha.Fanger@gmx.de>
2018-12-18 09:29:21 +0100

    test change of return value of day_of_the_year from 0 to 1

--- main.c
+++ main.c
@@ -7,14 +7,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// I was here - SFanger
+
 /**
  *
  *
  **/
 int day_of_the_year(int day, int month, int year)
 {
-
-    return 0;
+    // Returns now 1 instead of 0, what a change
+    return 1;
 }
 
 int main()
