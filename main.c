912c93194a37649f7fe38ed606b2b6d91d4939ff
Serjoscha Fanger <Serjoscha.Fanger@gmx.de>
2018-12-18 11:07:27 +0100

    added day_of_the_year , get_days_of_month and exits_date function

--- main.c
+++ main.c
@@ -7,14 +7,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_days_for_month(int month, int year);
+int exists_date(int day, int month, int year);
+
 /**
- *
- *
- **/
+* Name : day_of_the_year
+* Param : int day, int month, int year
+* Returns :
+*           the day of the year
+*          -1, if date invalid
+* Function : Returns the day of the year according to input
+**/
 int day_of_the_year(int day, int month, int year)
 {
-    // Returns now 1 instead of 0, what a change
-    return 1;
+    int days = 0;
+    if(exists_date(day,month,year))
+    {
+        for(int i = 0; i < month; i++)
+        {
+           days += get_days_for_month(i+1, year);
+        }
+        days -= get_days_for_month(month, year) - day;
+    }else
+    {
+        return -1;
+    }
+
+
+    return days;
 }
 
 /**
@@ -24,7 +44,7 @@ int day_of_the_year(int day, int month, int year)
 *           0, if year is not a leap year
 *           1, if year is a leap year
 *          -1, if year is smaller 1582
-* Funtion : Checks if year is a leap year in the gregorian calender
+* Function : Checks if year is a leap year in the gregorian calender
 **/
 int isLeapyear(int year)
 {
@@ -44,6 +64,77 @@ int isLeapyear(int year)
     {
         return 0;
     }
+}
+
+/**
+* Name : get_days_for_month
+* Param : int month, int year
+* Returns :
+*           days in given month/year
+*          -1, if month is invalid
+* Function : Gives back days of input month
+**/
+int get_days_for_month(int month, int year)
+{
+
+    int days = 0;
+    switch(month)
+    {
+        case 1 :
+        case 3 :
+        case 5 :
+        case 7 :
+        case 8 :
+        case 10:
+        case 12: days = 31;break;
+        case 2 :
+                 if(isLeapyear(year) == 1)
+                 {
+                     days = 29;break;
+                 }else if(isLeapyear(year) == 0)
+                 {
+                     days = 28;break;
+                 }else
+                 {
+                     return -1;
+                 }
+        case 4 :
+        case 6 :
+        case 9 :
+        case 11: days = 30;break;
+
+        default : return -1;
+    }
+    return days;
+}
+
+/**
+* Name : exists_date
+* Param : int day, int month, int year
+* Returns :
+*           0, if date is invalid
+*           1, if date is valid
+* Function : Checks if input date is in fact a date of the Gregorian calender between 1582 and 2400
+**/
+int exists_date(int day, int month, int year)
+{
+    if(year < 1582 || year > 2400)
+    {
+        return 0;
+    }else
+    {
+        if(get_days_for_month(month, year) == -1)
+        {
+            return 0;
+        }else if(get_days_for_month(month, year) < day || day <= 0)
+        {
+            return 0;
+        }else
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int main()
